282a0ca30a20751e28f6bba0a732acff2b3de1a1
alifrizqullahmaruf <125651171+alifrizqullahmaruf@users.noreply.github.com>
2023-06-20 22:31:03 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui"),require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
+}
+
